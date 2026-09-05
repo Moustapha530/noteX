b35e9ccca98088b20d99208f71fedee12912758d
Moustapha <moustaphamukwini@gmail.com>
2026-09-05 15:07:09 +0100

    add splash screen assets and update styles for launch themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:note_x/router.dart';
 
-void main() {
+Future<void> main() async {
+  final widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
+
+  FlutterNativeSplash.preserve(
+    widgetsBinding: widgetsBinding,
+  );
+
   runApp(
     const ProviderScope(
       child: MyApp(),
     ),
   );
+
+  await Future.delayed(
+    const Duration(seconds: 1),
+  );
+
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
