881345252da200458f3966c50a65975f5e11fcc4
Moustapha <moustaphamukwini@gmail.com>
2026-09-07 21:20:58 +0100

    apply the current setting theme in the app

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:note_x/router.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:note_x/theme.dart';
+import 'package:note_x/pages/settings/provider.dart';
 
 Future<void> main() async {
-  final widgetsBinding =
-      WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
 
   FlutterNativeSplash.preserve(
     widgetsBinding: widgetsBinding,
@@ -17,19 +20,35 @@ Future<void> main() async {
     ),
   );
 
-  
   FlutterNativeSplash.remove();
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final settings = ref.watch(settingsProvider);
+    
+    ThemeMode themeMode;
+    switch (settings.selectedTheme) {
+      case 'Light':
+        themeMode = ThemeMode.light;
+        break;
+      case 'Dark':
+        themeMode = ThemeMode.dark;
+        break;
+      default:
+        themeMode = ThemeMode.system;
+    }
+
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: router,
       title: 'noteX',
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: themeMode,
     );
   }
 }
